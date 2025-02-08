1b8cb123ceae96c8d0f7a43992c2231c171979d0
maverickOG <heysajit@gmail.com>
2025-02-08 16:26:43 +0530

    Leaderboard bug fixes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
-import Leaderboard from "./components/leaderboard";
+// import Leaderboard from "./components/leaderboard";
 import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
+        {/* <Route path="/leaderboard" element={<Leaderboard />} /> */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
