ab4b00f5453ea5b0c39b3be1b4cd521d1ee68241
ManaswiniN234 <manaswini.n234@gmail.com>
2025-02-08 14:34:30 +0530

    leaderboard test fix

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
+import Leaderboard from "./components/leaderboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/leaderboard" element={<Leaderboard/>} />
       </Routes>
     </Router>
   );
