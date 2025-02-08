300cd8be9fd8755f6f28473b4f2abbdcd0abee31
maverickOG <heysajit@gmail.com>
2025-02-08 10:27:49 +0530

    Create authentication with Clerk

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,26 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUp,
+  SignUpButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
 }
 
 export default App;
